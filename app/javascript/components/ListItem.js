09248f1cc4385c7352395d6de176a34618a87f6d
kevyntorres <kevyn.torres@gmail.com>
2021-02-07 14:39:17 +0000

    list buttons

--- app/javascript/components/ListItem.js
+++ app/javascript/components/ListItem.js
@@ -1,6 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
-import {Table} from "tabler-react"
+import {Table, Button} from "tabler-react"
 class ListItem extends React.Component {
     itemCols(items) {
         return (
@@ -16,6 +16,12 @@ class ListItem extends React.Component {
                 </Table.Col>
                 <Table.Col className="text-right text-muted d-none d-md-table-cell text-nowrap">
                     {items.category_id}
+                </Table.Col>
+                <Table.Col className="text-right d-none d-md-table-cell text-nowrap">
+                    <Button.List>
+                        <Button size="sm" outline color="primary">Edit</Button>
+                        <Button size="sm" outline color="danger">Delete</Button>
+                    </Button.List>
                 </Table.Col>
             </Table.Row>
         )
