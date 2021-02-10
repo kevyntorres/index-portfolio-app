da64fbb0dee12464296109a4c2971146cc8d62d8
kevyntorres <kevyn.torres@gmail.com>
2021-02-10 20:07:37 +0000

    category validate percent

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,10 @@
 class Category < ApplicationRecord
   has_many :items, dependent: :destroy
   validates_presence_of :name, :goal
+  validate :validate_goal_percent
+
+  def validate_goal_percent
+    total = Category.all.map(&:goal).sum + goal
+    errors.add(:company, 'Invalid Company ID') if total > 100
+  end
 end
