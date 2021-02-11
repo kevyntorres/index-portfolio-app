696cd496b66c700fdd3294c08649be907790cc3c
kevyntorres <kevyn.torres@gmail.com>
2021-02-11 20:27:36 +0000

    gemfile

--- db/migrate/20210122220844_create_operations.rb
+++ db/migrate/20210122220844_create_operations.rb
@@ -1,15 +1,15 @@
 class CreateOperations < ActiveRecord::Migration[6.0]
-  # def change
-  #   create_table :operations do |t|
-  #     t.references :asset, null: false, foreign_key: true
-  #     t.string :type
-  #     t.decimal :tax, precision: 10, scale: 2
-  #     t.integer :quantity
-  #     t.decimal :price, precision: 10, scale: 2
-  #     t.string :platform
-  #     t.datetime :operated_at
-  #
-  #     t.timestamps
-  #   end
-  #end
+  def change
+    create_table :operations do |t|
+      t.references :asset, null: false, foreign_key: true
+      t.string :type
+      t.decimal :tax, precision: 10, scale: 2
+      t.integer :quantity
+      t.decimal :price, precision: 10, scale: 2
+      t.string :platform
+      t.datetime :operated_at
+
+      t.timestamps
+    end
+  end
 end
