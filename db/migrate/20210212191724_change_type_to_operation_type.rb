e43361df255581d9819771b7268d416123f9a586
kevyntorres <kevyn.torres@gmail.com>
2021-02-12 21:36:58 +0000

    fixes and operations

--- db/migrate/20210212191724_change_type_to_operation_type.rb
+++
@@ -1,6 +0,0 @@
-class ChangeTypeToOperationType < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :operations, :type, :operation_type
-    rename_column :operations, :asset_id, :item_id
-  end
-end
