36aa9277f7ce5bd4352025035359f31c42e11d1d
kevyntorres <kevyn.torres@gmail.com>
2021-02-25 22:24:11 +0000

    add icons

--- app/javascript/components/Navbar.js
+++ app/javascript/components/Navbar.js
@@ -1,55 +1,69 @@
 import React from "react"
-import { Navbar, Nav } from 'react-bootstrap';
+import { Container, Navbar, Nav } from 'react-bootstrap';
+import { FiHome, FiFolder, FiRefreshCcw, FiBox } from "react-icons/fi";
+import { IconContext } from "react-icons";
 
 class NavbarComponent extends React.Component {
 
     navItems(items) {
         return (
-            items.map(item => <Nav.Link key={item.name} href={item.to}>{item.name} </Nav.Link>)
-        )
-
+            items.map(item => {
+                return(
+                    <IconContext.Provider value={{ className: "navbar-icons" }}>
+                        <Nav.Link key={item.to} href={item.to}>{item.icon} {item.name} </Nav.Link>
+                    </IconContext.Provider>
+                )
+            }))
     }
     render () {
     const itemsMenu = [
         {
             name: "Home",
-            icon: "home",
-            to: "/hello"
+            icon: <FiHome />,
+            to: "/"
         },
         {
             name: "Categories",
-            icon: "folder",
+            icon: <FiFolder />,
             to: "/categories"
         },
         {
             name: "Operations",
-            icon: "refresh-ccw",
+            icon: <FiRefreshCcw />,
             to: "/operations"
         },
         {
             name: "Assets",
-            icon: "box",
+            icon: <FiBox />,
             to: "/assets"
         }
     ]
     return (
-          <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
-              <Navbar.Brand href="/">
-                <img
-                  alt="IndexPortfolioApp"
-                  src="./assets/logo.jpeg"
-                  width="220"
-                  height="65"
-                  className="d-inline-block align-top"
-                />
-              </Navbar.Brand>
-              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-              <Navbar.Collapse id="responsive-navbar-nav">
-                  <Nav className="mr-auto">
-                      {this.navItems(itemsMenu)}
-                  </Nav>
-              </Navbar.Collapse>
-          </Navbar>
+        <>
+            <Navbar className="navbar-header">
+                <Container>
+                    <Navbar.Brand href="/">
+                        <img
+                            alt="IndexPortfolioApp"
+                            src="./assets/logo.jpeg"
+                            width="220"
+                            height="65"
+                            className="d-inline-block align-top"
+                        />
+                    </Navbar.Brand>
+                </Container>
+            </Navbar>
+            <Navbar className="navbar-header" collapseOnSelect expand="lg" variant="light">
+                <Container>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="mr-auto">
+                            {this.navItems(itemsMenu)}
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
     );
   }
 }
