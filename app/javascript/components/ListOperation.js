f68a3c2bf50a4bfb1575b38506f98e3ed37685ab
kevyntorres <kevyn.torres@gmail.com>
2021-02-28 00:01:03 +0000

    operations fix

--- app/javascript/components/ListOperation.js
+++ app/javascript/components/ListOperation.js
@@ -1,37 +1,36 @@
 import React from "react"
-import PropTypes from "prop-types"
-import {Button, Table} from "tabler-react";
+import {Button} from "tabler-react";
 class ListOperation extends React.Component {
   itemCols(items) {
     return (
-            <Table.Row>
-              <Table.Col>
+            <tr>
+              <td>
                 {items.id}
-              </Table.Col>
-              <Table.Col className="text-muted d-none d-md-table-cell text-nowrap">
+              </td>
+              <td className="text-muted d-none d-md-table-cell text-nowrap">
                 {items.item_id}
-              </Table.Col>
-              <Table.Col className="text-muted d-none d-md-table-cell text-nowrap">
+              </td>
+              <td className="text-muted d-none d-md-table-cell text-nowrap">
                 {items.operations_type}
-              </Table.Col>
-              <Table.Col className="text-muted d-none d-md-table-cell text-nowrap">
+              </td>
+              <td className="text-muted d-none d-md-table-cell text-nowrap">
                 {items.tax}
-              </Table.Col>
-              <Table.Col className="text-muted d-none d-md-table-cell text-nowrap">
+              </td>
+              <td className="text-muted d-none d-md-table-cell text-nowrap">
                 {items.quantity}
-              </Table.Col>
-              <Table.Col className="text-muted d-none d-md-table-cell text-nowrap">
+              </td>
+              <td className="text-muted d-none d-md-table-cell text-nowrap">
                 {items.price}
-              </Table.Col>
-              <Table.Col className="text-muted d-none d-md-table-cell text-nowrap">
+              </td>
+              <td className="text-muted d-none d-md-table-cell text-nowrap">
                 {items.platform}
-              </Table.Col>
-              <Table.Col className="d-none d-md-table-cell text-nowrap">
+              </td>
+              <td className="d-none d-md-table-cell text-nowrap">
                 <Button.List>
                   <Button size="sm" outline color="primary">Edit</Button>
                 </Button.List>
-              </Table.Col>
-            </Table.Row>
+              </td>
+            </tr>
     )
   }
   render () {
